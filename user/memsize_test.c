47290068b41c85fa23757c58dc06352afe87b8df
Omri Krispil <norvezzo@gmail.com>
2025-04-16 17:59:24 +0000

    Task 2: Implement memsize system call and test program

---
+++ user/memsize_test.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main() {
+  printf("Memory size: %d bytes\n", memsize());
+
+  void *p = malloc(20000);
+  if(p == 0){
+    printf("Failed to allocate memory\n");
+    exit(1);
+  }
+
+  printf("After malloc: %d bytes\n", memsize());
+
+  free(p);
+
+  printf("After free: %d bytes\n", memsize());
+
+  exit(0);
+}
